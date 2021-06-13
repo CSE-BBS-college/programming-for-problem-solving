cb3bf9e97add550073e98803c698d05a559f9327
mabhishek7081 <mabhishek7081@zohomail.in>
2021-06-13 14:45:30 +0530

    data_types and variables

--- Abhishek/01_printf.c
+++ Abhishek/01_printf.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
  int main() {
+
+     // simple string printing 
+     // \n is used for line brake
+     
      printf("Hello world! \n");
-     printf("This is a sentance \n");
-    
+     printf("This is a sentance \n");    
      return 0;
  }
---
+++ Abhishek/02_variables.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+    int main(int argc, char const *argv[])
+    {
+        /*
+        * data type int is used for integers. 
+        * data type float is used for floating values
+        * data type char is used for a charecter
+        *   
+        *  eg: 
+        * 
+        *    data_type  variable_name =  var_value;
+        *    int   age = 26;
+        *   
+        *  while printing the value we have to use format specifiers at place of variables
+        *   > %d is format specifier for integers
+        *   > %f for floating valuers
+        *   > %c for charecters
+        *   > %s for string
+        *
+        *  eg: 
+        *      printf("format specifier", variable_name);
+        *      printf("%d", age); 
+        *
+        */
+
+
+            int number = 3;
+            printf("%d \n",number);
+
+            // floating value
+            float percentage = 68.3;
+            printf("%f \n", percentage);
+
+            // charecter
+            char first_letter_of_name = 'A' ;    // charecter always needs to be in single quotestion marks
+            printf("%c \n", first_letter_of_name);
+
+
+
+        /* for using string we have to use a arrey of charecter
+        *   eg:    char name[8] = "abhishek";
+        *   
+        *       printf("%s \n ", name)
+        */
+
+            char name[] = "abhishek";
+            printf("%s \n ", name) ;
+
+        return 0;
+    }

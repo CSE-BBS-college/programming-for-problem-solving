b6c74850d66622cf9514711cdc06ca97fe9b9e97
mabhishek7081 <mabhishek7081@zohomail.in>
2021-06-13 14:49:33 +0530

    refactoring

--- Abhishek/01_printf.c
+++ Abhishek/01_printf.c
@@ -4,8 +4,12 @@
 
      // simple string printing 
      // \n is used for line brake
-     
+
      printf("Hello world! \n");
      printf("This is a sentance \n");    
+
+     printf("8 \n"); // printging a integer
+
+     printf("68.33 \n "); // printging a floating value
      return 0;
  }
